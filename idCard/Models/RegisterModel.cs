fb76d716d92a6f9c8a801c8113856b69dc6f8ff9
kerols1234 <kerolssamer348@gmail.com>
2022-03-27 13:31:35 +0200

    add account controller

---
+++ idCard/Models/RegisterModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace idCard.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(11, ErrorMessage = "Invalid Mobile Number", MinimumLength = 11)]
+        [RegularExpression(@"^01([0-9]{9})", ErrorMessage = "Invalid Mobile Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ idCard/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace idCard.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(11, ErrorMessage = "Invalid Mobile Number", MinimumLength = 11)]
+        [RegularExpression(@"^01([0-9]{9})", ErrorMessage = "Invalid Mobile Number")]
+        public string PhoneNumber { get; set; }
+
+    }
+}
